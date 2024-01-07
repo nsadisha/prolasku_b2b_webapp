365c0067f2b3d8e93335c45f40a7b8944569e438
Sadisha Nimsara <nsadisha@gmail.com>
2024-01-07 14:47:32 +0530

    added cart and checkout buttons to floating cart modal

--- public_html/pages/floating-cart.php
+++ public_html/pages/floating-cart.php
@@ -11,11 +11,15 @@ $output = '';
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Cart</h5>
+                <h5 class="modal-title"><?= $trans->getTrans('Cart'); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <ul class="cart-item-list" id="floating-cart-items"></ul>
+            </div>
+            <div class="modal-footer">
+                <a href="cart.php" class="btn btn-bold px-4"><?= $trans->getTrans('view cart'); ?></a>
+                <a href="checkout.php" class="btn btn-bold checkout-btn px-4"><?= $trans->getTrans('checkout'); ?></a>
             </div>
         </div>
     </div>
